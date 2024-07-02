281db8b9e489dbbbc49c1a82aa25f623dd650602
B0neh3ad <js1044k@gmail.com>
2024-07-02 19:24:41 +0900

    Add functions to show image & kspace slice

--- utils/common/utils.py
+++ utils/common/utils.py
@@ -9,6 +9,7 @@ import h5py
 import numpy as np
 import torch
 import random
+from matplotlib import pyplot as plt
 
 def save_reconstructions(reconstructions, out_dir, targets=None, inputs=None):
     """
@@ -54,3 +55,36 @@ def seed_fix(n):
     torch.backends.cudnn.benchmark = False
     np.random.seed(n)
     random.seed(n)
+
+
+def show_kspace_slice(kspace_path, slice_idx=0, coil_idxs=[0, 5, 10], cmap=None):
+    # convert kspace slice to real image
+    hf = h5py.File(kspace_path)
+    kspace_slice = hf['kspace'][slice_idx]
+    kspace_slice = np.log(np.abs(kspace_slice) + 1e-9)
+
+    kspace_title = kspace_path.split('/')[-1]
+
+    plt.figure()
+    plt.axis('off')
+    plt.title(f'{kspace_title} - slice {slice_idx}')
+    for i, coil_idx in enumerate(coil_idxs):
+        ax = plt.subplot(1, len(coil_idxs), i + 1)
+        plt.title(f'coil {coil_idx}')
+        ax.imshow(kspace_slice[coil_idx], cmap=cmap)
+
+
+def show_image_slices(img_path, slice_idxs=[0], cmap='gray'):
+    img_keys = ['image_grappa', 'image_input', 'image_label']
+    img_title = img_path.split('/')[-1]
+
+    for i, slice_idx in enumerate(slice_idxs):
+        hf = h5py.File(img_path)
+        plt.figure()
+        plt.title(f'{img_title} - slice {slice_idx}')
+        plt.axis('off')
+        for j, key in enumerate(img_keys):
+            ax = plt.subplot(1, len(img_keys), j + 1)
+            img_slice = hf[key][slice_idx]
+            plt.title(key)
+            ax.imshow(img_slice, cmap=cmap)
