Fix: fix some bugs and remove gradient checkpointing

utils/learning/train_part.py
@@ -29,19 +29,21 @@ def train_epoch(args, epoch, model, data_loader, optimizer, loss_type):
         target = target.cuda(non_blocking=True)
         maximum = maximum.cuda(non_blocking=True)
 
+        optimizer.zero_grad()
+
         # automatic mixed precision (amp)
         with torch.autocast(device_type="cuda"):
             output = model(kspace, mask)
-            loss = loss_type(output, target, maximum) / args.iters_to_accumulate
+            loss = loss_type(output, target, maximum) / args.iters_to_grad_acc
 
         # gradient scaling
         scaler.scale(loss).backward()
 
         # gradient accumulation
         if (iter + 1) % args.iters_to_grad_acc == 0:
-            optimizer.zero_grad()
             # gradient clipping
-            torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=args.grad_clip)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=args.grad_clip)
+
             scaler.step(optimizer)
             scaler.update()
 

utils/model/unet.py
@@ -78,16 +78,16 @@ class Unet(nn.Module):
 
         # apply down-sampling layers
         for layer in self.down_sample_layers:
-            output = checkpoint(layer, output)
+            output = layer(output)
             stack.append(output)
             output = F.avg_pool2d(output, kernel_size=2, stride=2, padding=0)
 
-        output = checkpoint(self.conv, output)
+        output = self.conv(output)
 
         # apply up-sampling layers
         for transpose_conv, conv in zip(self.up_transpose_conv, self.up_conv):
             downsample_layer = stack.pop()
-            output = checkpoint(transpose_conv, output)
+            output = transpose_conv(output)
 
             # reflect pad on the right/botton if needed to handle odd input dimensions
             padding = [0, 0, 0, 0]
@@ -99,7 +99,7 @@ class Unet(nn.Module):
                 output = F.pad(output, padding, "reflect")
 
             output = torch.cat([output, downsample_layer], dim=1)
-            output = checkpoint(conv, output)
+            output = conv(output)
         
         return output
 