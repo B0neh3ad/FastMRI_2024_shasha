75c0190b9c9f04edf5aa383f6763d8d5f37db13e
B0neh3ad <js1044k@gmail.com>
2024-07-06 11:38:58 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -12,6 +12,15 @@ python3.8 train.py \
   -n 'test_Varnet' \
   -t "$DATA_DIR_PATH/train/" \
   -v "$DATA_DIR_PATH/val/" \
-  --cascade 6 \
-  --chans 15 \
-  --sens_chans 4
+  --aug_on \
+  --mask_aug_on \
+  --aug_strength 0.5 \
+  --debug \
+
+#  --cascade 6 \
+#  --chans 15 \
+#  --sens_chans 4
+
+# 사용 금지 option 목록
+# --amp: loss 값이 이상해짐
+# --aug-weight-rot90: MRAugment의 rot90을 이용한 90도 회전 시 가로 세로 길이가 바뀌어 mask 사용 불가
