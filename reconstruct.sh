7861e051a7499daf2c5956ccc35e1654162272a5
B0neh3ad <js1044k@gmail.com>
2024-08-22 19:38:37 +0900

    Add first model reconstruction in reconstruct.sh

--- reconstruct.sh
+++ reconstruct.sh
@@ -4,6 +4,14 @@ if [ -f .env ]; then
   export $(cat .env | grep -v '^#' | xargs)
 fi
 
+python3.8 reconstruct.py \
+  -b 2 \
+  -n "varnet" \
+  -p "$DATA_DIR_PATH/leaderboard" \
+  --cascade 6 \
+  --chans 15 \
+  --sens_chans 4
+
 python3.8 reconstruct_2.py \
   -b 2 \
   -n "nafnet" \
